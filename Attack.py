ef0e4baa942845c16fa988ac480117503ce300d3
Chris Mazzullo <chris.mazzullo@gmail.com>
2013-12-30 23:24:13 -0500

    Added an Attack class

---
+++ Attack.py
@@ -0,0 +1,6 @@
+class Attack:
+	def __init__(self, base_dmg, _types, _stat_mod, _status_effect):
+		self.base_dmg = base_dmg
+		self.types = _types
+		self.stat_mod = _stat_mod
+		self.status_effect = _status_effect
