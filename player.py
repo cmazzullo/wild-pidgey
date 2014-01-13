f7723759d5bd851ca79f565afdd314fd0eb7569a
Mohammad H. Salim <mo_h_salim@hotmail.com>
2014-01-12 20:55:06 -0500

    added player class and updated flow of game drive

---
+++ player.py
@@ -0,0 +1,35 @@
+from attack import Attack
+from monster import Monster
+
+class Player():
+
+    def __init__(self, name):
+        self.name = name
+        self.monsters = []
+        self.lead = None
+        self.current_attack = None
+
+    def set_monsters(self, monsters):
+        if len(monsters) > 6 or len(monsters) <= 0:
+            return False
+        self.monsters = []
+        for m in monsters:
+            self.monsters.append(m)
+        self.lead = self.monsters[0]
+        return True
+
+    def make_move(self, move, choice):
+        if move == "switch":
+            self.lead = choice
+            self.current_attack = None
+        elif move == "attack":
+            self.current_attack = choice
+
+    def has_lost(self):
+        for m in self.monsters:
+            if not(m.hp <= 0):
+                return False
+        return True
+            
+            
+            
--- test.py
+++ test.py
@@ -1,8 +1,27 @@
 from attack import Attack
 from monster import Monster
+from player import Player
 
 
 def main():
+
+    #player 1 and player 2 will select their monsters then click start
+
+    #pre-battle screen will show both players monsters but will not reveal moves/states or which monster will be first.
+    #players pick which monster they want to lead with.  battle starts when people players click battle button
+
+    #select between attacking (which also gives an option of in which state) or switch to different monster
+    #once a choice is made, it's locked in.
+
+    #battle actions happen.  results are updated on screen.
+
+    #next move and so forth until a one (or possibly both) players lose all of his/her monsters.
+
+
+    player1 = Player("Mazzulo")
+    player2 = Player("Salim")
+
+    
 
     mud_blast = Attack(85.0, "earth", "intellectual", "", "")
     tornado_spin = Attack(70.0, "air", "physical", "", "")
@@ -10,6 +29,10 @@ def main():
 
     mazzullo = Monster("Mazzulo", ["air", "fire"], ["plasma", "solid"], [tornado_spin], 80.0, 50.0, 95.0, 75.0, 75.0, 70.0, 60.0, 60.0 )
     salim = Monster("Salim", ["earth", "water"], ["liquid", "gas", "solid"], [mud_blast], 75.0, 65.0, 75.0, 50.0, 95.0, 70.0, 30.0, 90.0 )
+
+    player1.set_monsters([mazzullo])
+    player2.set_monsters([salim])
+    
 
     print mazzullo.name + " hp = " + str(mazzullo.hp)
     print salim.name + " hp = " + str(salim.hp)
