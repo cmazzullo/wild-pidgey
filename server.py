6548900ff50e46f2046d4b560f8f5ee486b25b6f
Mohammad H. Salim <mo_h_salim@hotmail.com>
2014-01-19 22:29:04 -0500

    fixed merge/sync issue in server.py and also fixed a bug

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12388                # Reserve a port for your service.
+port = 12387                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
 print ("Starting up s(" + str(s) + ") host(" + str(host) + ") port(" + str(port) + ")")
@@ -22,36 +22,22 @@ while True:
 #for player2
 s.listen(5)                 # Now wait for client connection.
 while True:
-<<<<<<< HEAD
    conn2, addr2 = s.accept()     # Establish connection with client.
    if conn2 and addr2:
        print 'Got connection from ', addr2
        conn2.send('Thank you for connecting')
        break
-=======
-   c, addr = s.accept()     # Establish connection with client.
-   s.listen(5)              # Wait for a second client
-   c2, addr2 = s.accept()
-   if c and addr:
-       print '(Client 1: Got connection from ', addr
-       c.send('Thank you for connecting')
-   if c2 and addr2:
-       print '(Client 2: Got connection from ', addr2
-       c2.send('Thank you for connecting as well')
->>>>>>> ea069f00179b49fb9f713d6967c2578eaaedb755
 
 
 while True:
     print "Asking client for name..."
     name1 = conn1.recv(1024)
     name2 = conn2.recv(1024)
-    if name and name2:
+    if name1 and name2:
         conn1.send("Nice to meet you " + name1)
         conn2.send("Nice to meet you " + name2)
         break
 
-
-<<<<<<< HEAD
 
 while True:
    closeInput = raw_input("Type close to end all connections:  ")
@@ -61,9 +47,3 @@ while True:
       conn2.close()
       print ("connection closed for " + str(addr1) + " and " + str(addr2))
       break
-
-=======
-print "about to close..."
-c.close()                  # Close the connection
-print ("connection closed for " + str(addr))
->>>>>>> ea069f00179b49fb9f713d6967c2578eaaedb755
