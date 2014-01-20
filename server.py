8b6528af740e0878fd701f3a90734bde183f60c4
Chris Mazzullo <chris.mazzullo@gmail.com>
2014-01-19 22:23:34 -0500

    Deleted stray swap files

--- server.py
+++ server.py
@@ -9,11 +9,11 @@ s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
 while True:
-   c, addr = s.accept()     # Establish connection with client.
+   c1, addr1 = s.accept()     # Establish connection with client.
    s.listen(5)              # Wait for a second client
    c2, addr2 = s.accept()
-   if c and addr:
-       print '(Client 1: Got connection from ', addr
+   if c1 and addr1:
+       print '(Client 1: Got connection from ', addr1
        c.send('Thank you for connecting')
    if c2 and addr2:
        print '(Client 2: Got connection from ', addr2
@@ -21,11 +21,13 @@ while True:
 
 while True:
     print "Asking client for name..."
-    name = c.recv(1024)
-    if name:
-        c.send("Nice to meet you " + name)
+    player1_name = c1.recv(1024)
+    player2_name = c2.recv(1024)
+    if player1_name:
+        c1.send("Nice to meet you " + player1_name)
+    if player2_name:
+        c2.send("Nice to meet you " + player2_name)
         break
-
 
 print "about to close..."
 c.close()                  # Close the connection
