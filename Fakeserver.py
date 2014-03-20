60c0083e1b1df59c65aa43c0aedc41395233d0d6
Chris Mazzullo <chris.mazzullo@gmail.com>
2014-03-19 20:49:30 -0400

    Added networked client/server

--- Fakeserver.py
+++ Fakeserver.py
@@ -1,63 +1,94 @@
-import Engine
+import socket
 
+# these contain all important information about the connected players
+# playerN = (name, connection, number) where number is 1 or 2
+
+
+
+# waits for a player to connect
+# returns a nested tuple containing both client sockets
+# in the form (conn1, conn2)
+# this is unpacked in the Engine script
+def connect():
+    HOST = ''
+    PORT = 65000
+    
+    # socket 1, s1
+    temp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    temp.bind((HOST, PORT))
+    temp.listen(1)
+    (tempconn, addr) = temp.accept()
+    tempconn.sendall('65001')
+    tempconn.close()
+
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s1.bind((HOST, PORT + 1))
+    s1.listen(1)
+    (conn1, addr) = s1.accept()
+
+    temp.listen(1)
+    (tempconn, addr) = temp.accept()
+    tempconn.sendall('65002')
+    tempconn.close()
+    temp.close()
+
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s2.bind((HOST, PORT + 2))
+    s2.listen(1)
+    (conn2, addr) = s2.accept()
+
+    conn1.sendall('client 1 connected')
+    conn1.recv(1024)
+    conn2.sendall('client 2 connected')
+    conn2.recv(1024)
+
+    return (conn1, conn2)
+#   return ((conn1, s1), (conn2, s2))
+
+# takes connected socket
+# gets player name from client
+def get_player_name(conn):
+    conn.sendall('get_name')
+    return conn.recv(1024)
+
+# takes connected socket
 # prompts for action choice
 # returns the name of the choice
-def choose_action(player):
-    print str.format("{}'s turn:",player.name)
-    s = raw_input('Do you want to:\n1. Attack\n2. Switch\n3. Change State\n')
-    if s == '1':
-        return 'attack'
-    elif s == '2':
-        return 'switch'
-    elif s == '3':
-        return 'state'
+def choose_action((player, conn)):
+    conn.sendall('choose_action')
+    return conn.recv(1024)
 
 # prompts for attack choice
-# returns attack
-def choose_attack(player, attacks):
+# returns attack chosen
+def choose_attack((player, conn)):
+    attack_list = 'choose_attack'
     c = 1
-    print 'Which attack:\n'
     for a in attacks:
-        print (str(c) + '. ' + a.name)
+        attack_list = str(c) + '. ' + attack_list + a.name + '\n'
         c += 1
-    s = raw_input()
-    if s == '1':
-        return attacks[0]
-    elif s == '2':
-        return attacks[1]
-    elif s == '3':
-        return attacks[2]
-    elif s == '4':
-        return attacks[3]
+    conn.sendall(attack_list)
+    n =  conn.recv(1024)
+    return player.attacks[int(n)]
 
 # prompts for lead choice
 # returns lead
-def choose_lead(player, monsters):
+def choose_lead((player, conn)):
+    lead_list = 'choose_lead'
     c = 1
-    print 'Which lead:\n'
     for m in monsters:
-        print (str(c) + '. ' + m.name)
+        lead_list = lead_list + str(c) + '. ' + m.name + '\n'
         c += 1
-    s = raw_input()
-    if s == '1':
-        return monsters[0]
-    elif s == '2':
-        return monsters[1]
-    elif s == '3':
-        return monsters[2]
-    elif s == '4':
-        return monsters[3]
+    n = conn.recv(1024)
+    return player.monsters[int(n)]
 
 # prompts for state choice
 # returns state
-def choose_state(player):
-    print 'Which state:\n1. Solid\n2. Liquid\n3. Gas\n4. Plasma\n'
-    s = raw_input()
-    if s == '1':
-        return 'solid'
-    elif s == '2':
-        return 'liquid'
-    elif s == '3':
-        return 'gas'
-    elif s == '4':
-        return 'plasma'
+def choose_state((player, conn)):
+    conn.sendall('choose_state')
+    states = ('solid', 'liquid', 'gas', 'plasma')
+    n = conn.recv(1024)
+    return states[int(n)]
+
+def send_state(state, (p1, conn1), (p2, conn2)):
+    conn1.sendall('state' + state)
+    conn2.sendall('state' + state)
